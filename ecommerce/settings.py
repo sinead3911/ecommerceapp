9257ae86e59cd91eb4eb941c2fb3cc5fc141a57a
Sinead O'Neill <sinead-391@live.ie>
2017-11-28 16:18:41 +0000

    commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -23,11 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", True)
 
-ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME')]
-
-#[ 'ecommerceapp-sinead391.c9users.io']
+ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), 'ecommerceapp-sinead391.herokuapp.com']
 
 # Application definition
 
